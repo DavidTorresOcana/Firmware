f64a8bad4fddcd2f72a49d628eb0f06d69fc1c3f
DavidTorresOcana <david.torres@dbxdrones.com>
2016-02-13 13:35:22 +0100

    Change names of files Completing some headers and info in the files.

--- src/modules/dbx_control/dbx_control_params.c
+++ src/modules/dbx_control/dbx_control_params.c
@@ -1,37 +1,10 @@
-/****************************************************************************
+/**
+ * @file dbx_control_params.c
+ * Parameter definition for DBX Control.
  *
- *   Copyright (c) 2013, 2014 PX4 Development Team. All rights reserved.
+ * @author David Torres <david.torres@dbxdrones.com>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
-#include <nuttx/config.h>
+ */
 #include <systemlib/param/param.h>
 
 /**
@@ -41,7 +14,7 @@
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_Throtle_sens, 5.0f);
 
@@ -52,7 +25,7 @@ PARAM_DEFINE_FLOAT(DBX_Throtle_sens, 5.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_Yaw_sens, 20.0f);
 
@@ -63,7 +36,7 @@ PARAM_DEFINE_FLOAT(DBX_Yaw_sens, 20.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_Atti_sens, 10.0f);
 
@@ -74,7 +47,7 @@ PARAM_DEFINE_FLOAT(DBX_Atti_sens, 10.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_phi_tau, 0.7f);
 /**
@@ -84,7 +57,7 @@ PARAM_DEFINE_FLOAT(DBX_phi_tau, 0.7f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_phi_K_b, 3.0f);
 
@@ -95,7 +68,7 @@ PARAM_DEFINE_FLOAT(DBX_phi_K_b, 3.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_phi_f_i, 0.2f);
 
@@ -107,7 +80,7 @@ PARAM_DEFINE_FLOAT(DBX_phi_f_i, 0.2f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_theta_tau, 0.7f);
 /**
@@ -117,7 +90,7 @@ PARAM_DEFINE_FLOAT(DBX_theta_tau, 0.7f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_theta_K_b, 3.0f);
 
@@ -128,7 +101,7 @@ PARAM_DEFINE_FLOAT(DBX_theta_K_b, 3.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_theta_f_i, 0.2f);
 
@@ -139,7 +112,7 @@ PARAM_DEFINE_FLOAT(DBX_theta_f_i, 0.2f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_psi_tau, 4.0f);
 /**
@@ -149,7 +122,7 @@ PARAM_DEFINE_FLOAT(DBX_psi_tau, 4.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_psi_K_b, 2.5f);
 
@@ -160,7 +133,7 @@ PARAM_DEFINE_FLOAT(DBX_psi_K_b, 2.5f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_psi_f_i, 0.1f);
 
@@ -171,7 +144,7 @@ PARAM_DEFINE_FLOAT(DBX_psi_f_i, 0.1f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_p_tau, 0.3f);
 
@@ -182,7 +155,7 @@ PARAM_DEFINE_FLOAT(DBX_p_tau, 0.3f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_p_K_b, 6.0f);
 
@@ -193,7 +166,7 @@ PARAM_DEFINE_FLOAT(DBX_p_K_b, 6.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_q_tau, 0.3f);
 
@@ -204,7 +177,7 @@ PARAM_DEFINE_FLOAT(DBX_q_tau, 0.3f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_q_K_b, 6.0f);
 
@@ -215,7 +188,7 @@ PARAM_DEFINE_FLOAT(DBX_q_K_b, 6.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_r_tau, 0.5f);
 
@@ -226,7 +199,7 @@ PARAM_DEFINE_FLOAT(DBX_r_tau, 0.5f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_r_K_b, 5.0f);
 
@@ -238,7 +211,7 @@ PARAM_DEFINE_FLOAT(DBX_r_K_b, 5.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Advanced Controller
  */
 PARAM_DEFINE_FLOAT(DBX_Flaps_deg, 0.0f);
 
@@ -249,7 +222,7 @@ PARAM_DEFINE_FLOAT(DBX_Flaps_deg, 0.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_theta_Kp, 3.0f);
 
@@ -260,7 +233,7 @@ PARAM_DEFINE_FLOAT(DBXcl_theta_Kp, 3.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_phi_Kp, 3.0f);
 
@@ -271,7 +244,7 @@ PARAM_DEFINE_FLOAT(DBXcl_phi_Kp, 3.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_theta_Ki, 0.1f);
 
@@ -282,7 +255,7 @@ PARAM_DEFINE_FLOAT(DBXcl_theta_Ki, 0.1f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_phi_Ki, 0.1f);
 
@@ -293,7 +266,7 @@ PARAM_DEFINE_FLOAT(DBXcl_phi_Ki, 0.1f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_theta_Kd, 0.0f);
 
@@ -304,7 +277,7 @@ PARAM_DEFINE_FLOAT(DBXcl_theta_Kd, 0.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_phi_Kd, 0.0f);
 
@@ -315,7 +288,7 @@ PARAM_DEFINE_FLOAT(DBXcl_phi_Kd, 0.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_theta_dot_Kp, 0.2f);
 
@@ -326,7 +299,7 @@ PARAM_DEFINE_FLOAT(DBXcl_theta_dot_Kp, 0.2f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_phi_dot_Kp, 0.2f);
 
@@ -337,7 +310,7 @@ PARAM_DEFINE_FLOAT(DBXcl_phi_dot_Kp, 0.2f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_theta_dot_Ki, 0.0f);
 
@@ -348,7 +321,7 @@ PARAM_DEFINE_FLOAT(DBXcl_theta_dot_Ki, 0.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_phi_dot_Ki, 0.0f);
 
@@ -361,7 +334,7 @@ PARAM_DEFINE_FLOAT(DBXcl_phi_dot_Ki, 0.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_theta_dot_Kd, 0.0f);
 
@@ -372,6 +345,6 @@ PARAM_DEFINE_FLOAT(DBXcl_theta_dot_Kd, 0.0f);
  *
  * @min
  * @max
- * @
+ * @group DBX Classic Controller (PIDs)
  */
 PARAM_DEFINE_FLOAT(DBXcl_phi_dot_Kd, 0.0f);
